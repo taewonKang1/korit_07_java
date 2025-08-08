896a29d5021a244c694484b0cc7e6f8bbe43007d
taewonKang1 <edward0317@naver.com>
2025-08-08 17:46:43 +0900

    feat: 20250808 upcasting lesson

---
+++ src/main/java/ch15_casting/centralcontrol/CentralControlMain.java
@@ -0,0 +1,29 @@
+package ch15_casting.centralcontrol;
+
+public class CentralControlMain {
+    public static void main(String[] args) {
+        // 각 클래스의 객체 생성
+        Computer computer1 = new Computer();
+        AirConditioner airConditioner1 = new AirConditioner();
+        Speaker speaker1 = new Speaker();
+        // 참조자료형의 배열을 생성(비어있는거)
+//        Power[] powers = new Power[5];
+        // 이하의 코드는 powers 생성하면서 indexNumber가 고정되기 때문에
+        // 예시만 보여주고 안쓸겁니다.
+        Power[] powers = {new Computer(), new AirConditioner(), new Speaker()};
+
+        // 저희는 그래서 centralControl  객체를 만들면서 내부의 필드에 비어있는 Power[] 배열을
+        // 집어넣었습니다.
+        CentralControl centralControl = new CentralControl(
+                new Power[5]
+        );
+
+        centralControl.addDevice(computer1);        // 여기서 (암시적) 업캐스팅이 이루어졌습니다.
+        centralControl.addDevice(airConditioner1);
+        centralControl.addDevice(speaker1);
+        centralControl.addDevice(speaker1);
+        centralControl.addDevice(speaker1);
+        centralControl.addDevice(speaker1);
+
+    }
+}
